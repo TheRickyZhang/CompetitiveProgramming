8b205a782dc2948e3cd977fcb1a7cac94c9f796d
Ricky Zhang <rickyzhang196@outlook.com>
2025-01-16 16:50:57 -0500

    Begin Euler Tour

--- Notes_CIS4930/Notes/DP.cpp
+++ Notes_CIS4930/Notes/DP.cpp
@@ -47,3 +47,37 @@ f(i, n+1) {
     }
 }
 int res = accumulate(all(dp[n][0]), 0); // Every end pos state 0 mod m
+
+
+
+/* DP On Graphs
+Example - define a subtree* to be any set of connected vertices in a graph G (this is different from usual definition!).
+Find the number of subtrees of size <= k in graph.
+
+First, ignore k limitation.
+It is natural to define f(u) as the number of subtrees including u, and g(u) as the number of subtrees excluding u.
+    If the children of u are {v1, v2, ...}, then f(u) = MULT(f(v)+1), since we can pick any combination of f from children, including empty.
+    Then, g(u) = SUM(f(v) + g(v)), as without the root, we only pick from each of the children without combination.
+
+Now let's consider k. It is natural to extend f to f(u, k), counting subtrees of size k.
+    Then for f, we can still combine any number of f for children, but with the restriction that SUM(a) = k-1.
+    So, f(u, k) = SUM(PROD(f(v, a) | SUM(a) = k-1)
+    g remains similar as g(u, k) = SUM(f(v, k) + g(v, k)). We notice that the k can be dropped.
+
+All that's left to remain is to figure out how the f transition can be calculated efficiently. We can use traditional coin-sum DP:
+    Let dp[i][j] = first i children, current sum j. Then dp[i][j] = SUM(dp[i-1][j-k] * f(i, k) for all k={0, K})
+*/
+
+
+/*
+ *  Practice - minimum cost to delete nodes to a tree with k leafs
+ *
+ *  Let f = (u, k) be the minimum cost for the tree rooted at u to have k leaves
+ *      Our base case is f(u, 1) = 0 if deg(u) = 0
+ *      Then f(u, k) = MIN(SUM(f(v, a)) | SUM(a) = k)
+ *      But we should also consider the cost of deleting a node. Thus, f(u, 0) = SUM(c(u)) for the subtree
+ *
+ *  In this case, a separate function g is not required, as the state update is already addressed in f.
+ *
+ *  So, we use dp[i][j] = {index of v, num leafs} with transition dp[i][j] = MIN(dp[i-1][a-k] * f(i, k)) for k={1, K}
+ */
